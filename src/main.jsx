94566797602a71c3cb51b8bb354de95fcbe12360
Phong-Le-Duc <phongleduc@gmail.com>
2025-05-26 13:50:38 +0200

    added tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import './index.scss'
 import { RouterProvider } from 'react-router'
 import router from './router'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import queryClient from './queryClient'
 
-const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
   </StrictMode>,
 )
--- src/utilities/typicode.js
+++ src/utilities/typicode.js
@@ -1,9 +1,16 @@
-export async function getUsers() {
-    
-    const response = await fetch("https://jsnplaceholder.typicode.com/users");
+import queryClient from "../queryClient";
+
+export async function getUsers() {  
+    return queryClient.fetchQuery({
+        queryKey: ['users'],
+        queryFn: async function () {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!response.ok) {
         throw new Error({message:'Den er helt gal'});
     }
     
    return response.json();
+ }
+           
+      });   
 }
